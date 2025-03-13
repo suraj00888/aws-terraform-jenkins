adccc6884afae4ef0c59c368ff1998dcaed6f7e0
suraj00888 <surajmali430@gmail.com>
2025-03-13 13:50:23 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,9 +2,7 @@ output "ec2_public_ip" {
   value = aws_instance.example.public_ip
 }
 
-output "s3_bucket_name" {
-  value = aws_s3_bucket.example.bucket
-}
+
 
 output "rds_endpoint" {
   value = aws_db_instance.example.endpoint
