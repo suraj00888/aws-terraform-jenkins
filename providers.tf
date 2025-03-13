417d24f777d6f42eef0a48dde4d86c085fa7f4a8
suraj00888 <surajmali430@gmail.com>
2025-03-13 11:53:40 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0" 
     }
   }
 }
-
 provider "aws" {
   region = "us-east-1"
 }
